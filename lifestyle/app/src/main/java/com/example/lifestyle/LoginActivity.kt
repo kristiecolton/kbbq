77ac8fd4e2cfd6b8ca296fe2386a042e5a8ce2be
Bradenmcclean <mcclean.braden@gmail.com>
2021-09-17 14:50:20 -0600

    Added the login UI picture and fisrt name last name

--- lifestyle/app/src/main/java/com/example/lifestyle/LoginActivity.kt
+++ lifestyle/app/src/main/java/com/example/lifestyle/LoginActivity.kt
@@ -1,10 +1,18 @@
 package com.example.lifestyle
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View;
+import android.util.Log
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener{
 
@@ -16,13 +24,37 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener{
 
         mCreateAProfileBtn=findViewById(R.id.create_a_profile_btn) as Button;
         mCreateAProfileBtn.setOnClickListener(this);
-        print("Here We Godasdasdasaadadasdada:");
+        var dbManager : DBManager = DBManager(this);
+        var usersUIDArray=dbManager.getAllUid()
+        var linearLayoutUis=findViewById<LinearLayout>(R.id.users)
+
+        usersUIDArray.forEach{
+            val resBtn = ImageButton(this)
+            val tv = TextView(this)
+            tv.gravity = Gravity.CENTER or Gravity.BOTTOM
+            tv.text = dbManager.getFirstName(it) +" " +dbManager.getLastName(it)
+            var t=dbManager.getLastName(it)
+            Log.d("LOG", "WHY IS THIS SO DUMB: $t")
+            resBtn.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.humidity));
+            resBtn.setBackgroundColor(Color.TRANSPARENT);
+//            linearLayoutUis.addView(resBtn);
+            linearLayoutUis.addView(tv);
+
+        }
+
+
+
+        // Create a DBManager object
+
+
+
     }
 
     override fun onClick(v: View?) {
         if (v != null) {
             when (v.id) {
                 R.id.create_a_profile_btn->{
+
                     showCreateAProfileActivity()
                 }
                 else -> { // Note the block
