5465b70e4fbcee4b998f60745f3b0017cd375e64
bradenpotter <practicaldreamer24@gmail.com>
2021-09-21 16:20:59 -0600

    added error message if user leaves first or last name fields blank

--- lifestyle/app/src/main/java/com/example/lifestyle/FirstAndLastName.kt
+++ lifestyle/app/src/main/java/com/example/lifestyle/FirstAndLastName.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.findNavController
 import com.example.lifestyle.databinding.FragmentFirstAndLastNameBinding
 
@@ -27,10 +30,26 @@ class FirstAndLastName : Fragment() {
         binding.firstAndLastNameNextButton.setOnClickListener { view : View ->
             val firstName: String = binding.firstNameEt.text.toString()
             val lastName: String = binding.lastNameEt.text.toString()
-            passData(firstName, lastName)
-            view.findNavController().navigate(R.id.action_firstAndLastName_to_fragmentAgeHeightWeight) }
+
+            if (firstName == "" || lastName == "") {
+                hideMyKeyboard()
+                Toast.makeText(this.activity, "You must enter a First and Last Name", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                passData(firstName, lastName)
+                view.findNavController().navigate(R.id.action_firstAndLastName_to_fragmentAgeHeightWeight)
+            }
+        }
 
         return binding.root
+    }
+
+    fun hideMyKeyboard() {
+        val view = this.activity?.currentFocus
+        if (view != null) {
+            val hideMe = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideMe.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
     interface OnDataPass {
